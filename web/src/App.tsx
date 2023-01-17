07b1128e90369d543b05cbd9a25d92f8f6fba4d6
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-17 16:05:58 -0300

    Web Setup

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <div className="bg-zinc-900 w-24 h-16 text-white rounded text-center flex items-center justify-center">
+      Hello worl
+    </div>
+  )
+}
