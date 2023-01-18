9a0b401d0bea65e8de6db9bea742192a0a1c8f73
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-18 20:17:30 -0300

    component: Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
+import { Header } from './components/Header'
 import './styles/global.css'
 
 export function App() {
 
   return (
-    <div className="bg-zinc-900 w-24 h-16 text-white rounded text-center flex items-center justify-center">
-      Hello worl
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+      </div>
     </div>
   )
 }
