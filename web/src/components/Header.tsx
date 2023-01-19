7d67ded8c13231e70e8658b739c111b7c8169b3b
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-19 19:57:26 -0300

    component: Create habit modal

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Plus, X } from "phosphor-react";
 import { useState } from "react";
 import logoImage from '../assets/logo.svg'
 import * as Dialog from '@radix-ui/react-dialog';
+import { NewHabitForm } from "./NewHabitForm";
 
 export function Header() {
   return (
@@ -26,7 +27,12 @@ export function Header() {
             <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
               <X size={24} aria-label="Close" />
             </Dialog.Close>
-            Conteudo do modal
+            
+            <Dialog.Title className="text-3xl leading-tight font-extrabold">
+              Criar hábito
+            </Dialog.Title>
+
+            <NewHabitForm />
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
