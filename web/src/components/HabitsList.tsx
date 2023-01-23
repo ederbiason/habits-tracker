34af34d0d224558f8557b8ad7b6dbf07cc239cb5
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-23 18:00:33 -0300

    fix: Disabling the checkbox when the date passes

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -30,7 +30,7 @@ export function HabitsList({ date }: HabitsListProps) {
         })
     }, [])
 
-    
+    const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
 
     return (
         <div className='mt-6 flex flex-col gap-3'>
@@ -39,7 +39,7 @@ export function HabitsList({ date }: HabitsListProps) {
                     <Checkbox.Root
                         key={habit.id}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
-                        
+                        disabled={isDateInPast}
                         className='flex items-center gap-3 group'
                     >
                         <div className='w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
