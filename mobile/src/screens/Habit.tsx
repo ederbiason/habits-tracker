658ed8c01ba20a7ff933e84ffe1edf342b8e3d6e
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-01-24 15:27:14 -0300

    chore: Progress bar value

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -7,6 +7,7 @@ import { Checkbox } from '../components/Checkbox'
 import { useEffect, useState } from 'react'
 import { Loading } from '../components/Loading'
 import { api } from '../lib/axios'
+import { generateProgressPercentage } from '../utils/generate-progress-percentage'
 
 interface Params {
   date: string
@@ -31,6 +32,10 @@ export function Habit() {
   const parsedDate = dayjs(date)
   const dayOfWeek = parsedDate.format('dddd')
   const dayAndMonth = parsedDate.format('DD/MM')
+
+  const habitsProgress = dayInfo?.possibleHabits 
+    ? generateProgressPercentage(dayInfo.possibleHabits.length, completedHabits.length) 
+    : 0
 
   async function fetchHabits() {
     try {
@@ -88,7 +93,7 @@ export function Habit() {
           {dayAndMonth}
         </Text>
 
-        <ProgressBar progress={50} />
+        <ProgressBar progress={habitsProgress} />
 
         <View className='mt-6'>
           {
